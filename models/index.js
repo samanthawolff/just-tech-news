f9fef60d04bba69ec5109f9844072401529855ea
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-24 13:56:06 -0500

    finishing all api routes for the user model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
+const User = require('./Users');
 
 
 module.exports = { User };
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
-    User.findAll()
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
         console.log(err);
@@ -16,6 +18,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
