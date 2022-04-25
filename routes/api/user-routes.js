cb4e8793a2186ac1c2fd5efe7c9c61c697258733
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-24 20:47:55 -0500

    use hooks to hash password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,6 +55,7 @@ router.post('/', (req, res) => {
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
