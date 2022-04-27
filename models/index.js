a7fcb54a861b6da6a45e825f9c5de0166ac91c73
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-26 19:40:02 -0500

    GET all posts

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require('./Users');
 const Post = require('./Post') ;
 
+// Create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 
 module.exports = { User, Post };
