129b2640148e88a73dfd855a79adc09d2b702cd9
Samantha Wolff <samanthawolff07@gmail.com>
2022-05-03 15:12:04 -0500

    fixing plurals and date

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helper');
 
 const path = require('path');
 const express = require('express');
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
